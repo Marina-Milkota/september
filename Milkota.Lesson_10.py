052f93e9798a02171473aaf9b2bcd74b0c4505c5
Mari.Milkota <mari.milkota@gmail.com>
2022-09-20 12:15:12 +0300

    Lesson_10 исправила округления и ошибку пропускаввода числа,цикл повторяется,если не задана верная операция Lesson 12 выполнено задание по теме классы

--- Milkota.Lesson_10.py
+++ Milkota.Lesson_10.py
@@ -4,19 +4,22 @@
 # Пятая операция тоже через функцию (для выхода из калькулятора)
 # По желанию использовать декоратор
 
-def summ(a,b):print(f"Сумма чисел равна {a+b}")
-def diff(a,b):print(f"Разность чисел равна {a-b}")
-def mult(a,b):print(f"Произведение чисел равно {a*b}")
+def summ(a,b):print(f"Сумма чисел равна {round(a+b,1)}")
+def diff(a,b):print(f"Разность чисел равна {round(a-b,1)}")
+def mult(a,b):print(f"Произведение чисел равно {round(a*b,1)}")
 def div(a,b):
     try:
-        print(f"Деление чисел равно {a/b}")
+        print(f"Деление чисел равно {round(a/b,1)}")
     except ZeroDivisionError:print('На ноль делить нельзя!')
 
 while True:
     operation=input('Выберите операцию:+,-,*,/ (для выхода введите n) ')
     if operation=='n':break
+    elif operation=='':
+        continue
     elif operation not in '+-*/':
         print('Такого знака нет.Введите операцию корректно!')
+        continue
     try:
         number_1 = float(input('Введите первое число: '))
         number_2 = float(input('Введите второе число: '))
@@ -24,5 +27,5 @@ while True:
     if operation == '+': summ(number_1,number_2)
     elif operation == '-': diff(number_1,number_2)
     elif operation == '*':mult(number_1,number_2)
-    elif operation == '/':diff(number_1,number_2)
+    elif operation == '/':div(number_1,number_2)
 
